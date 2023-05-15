f31fe08fbd2d629fe3afb97d70e61af5c936bef7
pythonthm <pythonthm@gmail.com>
2023-05-15 15:23:08 +0900

    noto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,4 @@ export default defineNuxtConfig({
   //   app: {
   //     pageTransition: { name: "page", mode: "out-in" },
   //   },
-
-  app: {
-    head: {
-      link: [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap",
-          crossorigin: "",
-        },
-      ],
-    },
-  },
 });
