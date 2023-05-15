5bddc366183129a5940b717dda0731d2c248f6bb
pythonthm <pythonthm@gmail.com>
2023-05-15 11:57:15 +0900

    conference

--- pages/mayConference.vue
+++ pages/mayConference.vue
@@ -6,346 +6,185 @@
       </tr>
       <tr class="date">
         <th></th>
-        <th colspan="3">5月22日(月)</th>
-        <th colspan="3">5月23日(火)</th>
-        <th colspan="3">5月24日(水)</th>
-        <th colspan="3">5月25日(木)</th>
-        <th colspan="3">5月26日(金)</th>
+        <th class="color-one" colspan="3">5月22日(月)</th>
+        <th class="color-two" colspan="3">5月23日(火)</th>
+        <th class="color-three" colspan="3">5月24日(水)</th>
+        <th class="color-four" colspan="3">5月25日(木)</th>
+        <th class="color-five" colspan="3">5月26日(金)</th>
       </tr>
 
       <!-- 15:00～ -->
 
-      <tr class="fifteen">
+      <tr>
         <td rowspan="3">15:00</td>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-one">上村 樹生</td>
+          <td class="color-one">川倉</td>
+          <td class="color-one">所</td>
         </div>
         <div>
-          <td>森内 碧陸</td>
-          <td>津川</td>
-          <td>田原</td>
+          <td class="color-two">森内 碧陸</td>
+          <td class="color-two">津川</td>
+          <td class="color-two">田原</td>
         </div>
         <div>
-          <td>会田 梛</td>
-          <td>有賀</td>
-          <td>田原</td>
+          <td class="color-three">会田 梛</td>
+          <td class="color-three">有賀</td>
+          <td class="color-three">田原</td>
         </div>
         <div>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
         </div>
         <div>
-          <td>上嶋 望愛</td>
-          <td>佐藤</td>
-          <td>有賀</td>
-        </div>
-      </tr>
-      <tr class="fifteen">
-        <div>
-          <td>国重 紬希</td>
-          <td>津川</td>
-          <td>田原</td>
-        </div>
-        <div>
-          <td>橋本 悠杜</td>
-          <td>有賀</td>
-          <td>田原</td>
-        </div>
-        <div>
-          <td></td>
-          <td></td>
-          <td></td>
-        </div>
-        <div>
-          <td></td>
-          <td></td>
-          <td></td>
-        </div>
-        <div>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="color-five">上嶋 望愛</td>
+          <td class="color-five">佐藤</td>
+          <td class="color-five">有賀</td>
         </div>
       </tr>
-      <tr class="fifteen">
+      <tr>
         <div>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="color-one">国重 紬希</td>
+          <td class="color-one">津川</td>
+          <td class="color-one">田原</td>
         </div>
         <div>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="color-two">橋本 悠杜</td>
+          <td class="color-two">有賀</td>
+          <td class="color-two">田原</td>
         </div>
         <div>
-          <td></td>
-          <td></td>
-          <td></td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
         </div>
         <div>
-          <td>八田 空</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
         </div>
         <div>
-          <td>西垣 晴樹</td>
-          <td>川倉</td>
-          <td>佐藤</td>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
+        </div>
+      </tr>
+      <tr>
+        <div>
+          <td class="color-one">髙橋 蓮</td>
+          <td class="color-one">有賀</td>
+          <td class="color-one">伊藤</td>
+        </div>
+        <div>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
+        </div>
+        <div>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
+        </div>
+        <div>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
+        </div>
+        <div>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
         </div>
       </tr>
 
       <!-- 15:30から -->
-      <tr class="fifteenThree">
-        <td rowspan="3">15:30</td>
+      <tr>
+        <td rowspan="1">15:30</td>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-one"></td>
+          <td class="color-one"></td>
+          <td class="color-one"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-four">八田 空</td>
+          <td class="color-four">川倉</td>
+          <td class="color-four">所</td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
-      <tr class="fifteenThree">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-five">西垣 晴樹</td>
+          <td class="color-five">川倉</td>
+          <td class="color-five">佐藤</td>
         </div>
       </tr>
-      <tr class="fifteenThree">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
+
       <!-- 16:00から -->
-      <tr class="forteen">
-        <td rowspan="3">16:00</td>
+      <tr>
+        <td rowspan="1">16:00</td>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-one">志賀 楓馬</td>
+          <td class="color-one">川倉</td>
+          <td class="color-one">田原</td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-two">石井 悠幹</td>
+          <td class="color-two">所</td>
+          <td class="color-two">川倉</td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
+          <td class="color-three"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
+          <td class="color-four"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
-      <tr class="forteen">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
-      <tr class="forteen">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
+          <td class="color-five"></td>
         </div>
       </tr>
 
       <!-- 枠外 -->
-      <tr class="off">
-        <td rowspan="3">枠外</td>
+      <tr>
+        <td rowspan="1">枠外</td>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-one"></td>
+          <td class="color-one"></td>
+          <td class="color-one"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
+          <td class="color-two"></td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-three">24日(水)<br />菅原 瑠迦<br />16:30</td>
+          <td class="color-three">川倉</td>
+          <td class="color-three">田原</td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-four">29日(月)<br />北川 煌<br />15:00</td>
+          <td class="color-four">所</td>
+          <td class="color-four">佐藤</td>
         </div>
         <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
-      <tr class="off">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-      </tr>
-      <tr class="off">
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
-        </div>
-        <div>
-          <td>上村 樹生</td>
-          <td>川倉</td>
-          <td>所</td>
+          <td class="color-five">29日(月)<br />清水 玲志<br />16:00</td>
+          <td class="color-five">有賀</td>
+          <td class="color-five">津川</td>
         </div>
       </tr>
     </table>
@@ -356,17 +195,20 @@
   span {
     color: tomato;
   }
-  .fifteen {
-    background-color: #f0f8ff;
+  .color-one {
+    background-color: #ffd1d1;
   }
-  .fifteenThree {
-    background-color: blanchedalmond;
+  .color-two {
+    background-color: #ffd1e8;
   }
-  .forteen {
-    background-color: rgb(166, 207, 245);
+  .color-three {
+    background-color: #ffd1ff;
   }
-  .off {
-    background-color: grey;
+  .color-four {
+    background-color: #e8d1ff;
+  }
+  .color-five {
+    background-color: #d1d1ff;
   }
   .table-container {
     display: flex;
