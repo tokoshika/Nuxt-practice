0a1b924f2a7055caaeab62363b009a9190beb48f
pythonthm <pythonthm@gmail.com>
2023-05-16 13:03:03 +0900

    again

--- components/test.vue
+++ components/test.vue
@@ -163,11 +163,11 @@
 
 <style scoped>
   .date {
-    font-size: 1.2rem;
+    font-size: 1.5rem;
   }
   td {
-    font-size: 1.5rem;
-    padding: 3.5px;
+    font-size: 1.2rem;
+    padding: 3px;
     /* width: 60px; */
   }
   .meeting {
