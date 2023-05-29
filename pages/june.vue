690c00b3927e43bbcbdc0e5d7ece36098a7fb6f5
pythonthm <pythonthm@gmail.com>
2023-05-29 14:44:06 +0900

    add-june

--- pages/june.vue
+++ pages/june.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    【特別支援】名義貸し<span>早見表</span><br /><span>赤字</span
+    6月分【特別支援】名義貸し<span>早見表</span><br /><span>赤字</span
     >の名前は只野先生に貸し出されています。<br />必要な名義がありましたら、所の机の上にポストイットして下さい。
   </div>
   <div class="pla">
