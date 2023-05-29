8a9957192644d56e689b98ab25a668f88b03f727
pythonthm <pythonthm@gmail.com>
2023-05-29 14:40:06 +0900

    third

--- components/june/juneplan.vue
+++ components/june/juneplan.vue
@@ -1,3 +1,103 @@
 <template>
-  <div>This is June ClassPlan for Tadano</div>
+  <div class="table-container">
+    <table border="1">
+      <tr>
+        <th colspan="6"><h3>5月29日～6月2日</h3></th>
+      </tr>
+      <tr class="date">
+        <th></th>
+        <th>月</th>
+        <th>火</th>
+        <th>水</th>
+        <th>木</th>
+        <th>金</th>
+      </tr>
+      <tr class="date">
+        <th></th>
+        <th>29</th>
+        <th>30</th>
+        <th>31</th>
+        <th>1</th>
+        <th>2</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td><span>川倉</span></td>
+        <td></td>
+        <td>川倉</td>
+        <td><span>川倉</span></td>
+        <td><span>川倉</span></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td></td>
+        <td><span>川倉</span></td>
+        <td>川倉</td>
+        <td><span>川倉</span></td>
+        <td><span>川倉</span></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td></td>
+        <td><span>川倉</span></td>
+        <td>川倉</td>
+        <td><span>川倉</span></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td></td>
+        <td><span>川倉</span></td>
+        <td>川倉</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td><span>川倉</span></td>
+        <td><span>川倉</span></td>
+        <td>川倉</td>
+        <td><span>川倉</span></td>
+        <td><span>川倉</span></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td><span>川倉</span></td>
+        <td></td>
+        <td>川倉</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
 </template>
+
+<style scoped>
+  span {
+    color: tomato;
+  }
+  .need {
+    color: green;
+  }
+  .table-container {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+  }
+  table {
+    margin: 0;
+  }
+  .date {
+    height: 20px;
+  }
+
+  th {
+    background-color: rgb(236, 231, 231);
+  }
+  tr,
+  td {
+    height: 72px;
+    text-align: center;
+    padding: 6px;
+  }
+</style>
--- pages/june.vue
+++ pages/june.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
+    【特別支援】名義貸し<span>早見表</span><br /><span>赤字</span
+    >の名前は只野先生に貸し出されています。<br />必要な名義がありましたら、所の机の上にポストイットして下さい。
+  </div>
+  <div class="pla">
     <JuneJuneplan />
   </div>
-  <p>Getting Ready</p>
 </template>
 
 <style scoped>
-  p {
-    color: rgb(239, 69, 98);
+  .pla {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    justify-content: center;
+    background-color: whitesmoke;
+  }
+  span {
+    color: tomato;
+  }
+  @media screen and (min-width: 926px) {
+    .pla {
+      display: flex;
+      text-align: center;
+      justify-content: center;
+    }
+    .compoment {
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+    }
+    span {
+      color: tomato;
+    }
   }
 </style>
